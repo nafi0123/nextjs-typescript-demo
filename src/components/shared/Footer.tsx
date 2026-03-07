99002458c4ae19c6b8ce5c900346a918284ec7af
NAFI MAHMUD BUKHARI <nafimahmudbukhari@192.168.1.6>
2026-03-07 13:35:24 +0600

    first commit

---
+++ src/components/shared/Footer.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from './Container';
+
+const Footer = () => {
+    return (
+        <div>
+            <Container>
+                FFFFFF
+            </Container>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/components/shared/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Container from './Container';
+
+const Navbar = () => {
+    return (
+        <div>
+            <Container>
+                <h1>nnnn</h1>
+            </Container>
+        </div>
+    );
+};
+
+export default Navbar;
